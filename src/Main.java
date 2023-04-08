abeadc2e66655c32ec41f137d6e4229ddf9ada12
Buhanzaz <buhanza21@yandex.ru>
2023-04-09 00:43:40 +0300

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Поехали!
+        Scanner scanner = new Scanner(System.in);
+
+        printMenu();
+
+        while (true){
+            int command = scanner.nextInt();
+            switch (command) {
+                case 0:
+                    return;
+                case 1:
+                    System.out.println("Вы ввели неверное значение!");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Вы ввели неверное значение!");
+            }
+        }
+
+
+    }
+
+    public static void printMenu() {
+        System.out.println("1. Считать все месячные отчёты");
+        System.out.println("2. Считать годовой отчёт");
+        System.out.println("3. Сверить отчёты");
+        System.out.println("4. Вывести информацию о всех месячных отчётах");
+        System.out.println("5. Вывести информацию о годовом отчёте");
+        System.out.println("0. Выход");
     }
 }
 
